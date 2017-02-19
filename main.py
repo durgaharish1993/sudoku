ae3b402486d68a96a6bfd883ee97d4a1602d2c6f
dimbul <durgaharish1993@gmail.com>
2017-02-18 20:58:47 -0800

    Files added

---
+++ main.py
@@ -0,0 +1,116 @@
+
+def print_sudoku(list_sud):
+    for i in range(9):
+        for j in range(9):
+            print list_sud[i][j],
+        print ('\n')
+
+
+
+def find_empty_location(list_sud, l):
+    for row in range(9):
+        for col in range(9):
+            if (list_sud[row][col] == 0):
+                l[0] = row
+                l[1] = col
+                return True
+    return False
+
+
+# Returns a boolean which indicates whether any assigned entry
+# in the specified row matches the given number.
+def used_in_row(list_sud, row, num):
+    for i in range(9):
+        if (list_sud[row][i] == num):
+            return True
+    return False
+
+
+# Returns a boolean which indicates whether any assigned entry
+# in the specified column matches the given number.
+def used_in_col(list_sud, col, num):
+    for i in range(9):
+        if (list_sud[i][col] == num):
+            return True
+    return False
+
+
+# Returns a boolean which indicates whether any assigned entry
+# within the specified 3x3 box matches the given number
+def used_in_box(list_sud, row, col, num):
+    for i in range(3):
+        for j in range(3):
+            if (list_sud[i + row][j + col] == num):
+                return True
+    return False
+
+
+# Checks whether it will be legal to assign num to the given row,col
+# Returns a boolean which indicates whether it will be legal to assign
+# num to the given row,col location.
+def check_location_is_safe(list_sud, row, col, num):
+    # Check if 'num' is not already placed in current row,
+    # current column and current 3x3 box
+    return not used_in_row(list_sud, row, num) and not used_in_col(list_sud, col, num) and not used_in_box(list_sud, row - row % 3,
+                                                                                                 col - col % 3, num)
+
+
+def solve_sudoku(list_sud):
+    # 'l' is a list variable that keeps the record of row and col in find_empty_location Function
+    l = [0, 0]
+
+    # If there is no unassigned location, we are done
+    if (not find_empty_location(list_sud, l)):
+        return True
+
+    # Assigning list values to row and col that we got from the above Function
+    row = l[0]
+    col = l[1]
+
+    # consider digits 1 to 9
+    for num in range(1, 10):
+
+        # if looks promising
+        if (check_location_is_safe(list_sud, row, col, num)):
+
+            # make tentative assignment
+            list_sud[row][col] = num
+
+            # return, if sucess, ya!
+            if (solve_sudoku(list_sud)):
+                return True
+
+            # failure, unmake & try again
+            list_sud[row][col] = 0
+
+    # this triggers backtracking
+    return False
+
+
+# Driver main function to test above functions
+if __name__ == "__main__":
+
+
+    with open('evenMoreConsistent.txt','rb') as fp:
+        lines=fp.readlines()
+        data_dict = {}
+        count=0
+        temp_data = []
+        for line in lines:
+            if 'easy' in line or 'medium' in line or 'hard' in line  or 'evil' in line:
+                data_dict[count]=temp_data
+                count+=1
+                temp_data=[]
+                continue
+            else:
+                if line!='\n':
+                    temp_data += [map(int,list(line[:-1]))]
+
+
+
+
+    del data_dict[0]
+
+    for key in data_dict:
+        print key,solve_sudoku(data_dict[key])
+        print print_sudoku(data_dict[key])
